b62227acb7085eaf57cf69390c7c3c101406a713
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-05 15:39:26 -0300

    minor fix no status code de retorno para a exceção

--- src/main/java/br/com/aula4/tm/linktracker/exception/InvalidLinkExceptionHandler.java
+++ src/main/java/br/com/aula4/tm/linktracker/exception/InvalidLinkExceptionHandler.java
@@ -1,6 +1,7 @@
 package br.com.aula4.tm.linktracker.exception;
 
 import br.com.aula4.tm.linktracker.dto.ExceptionDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,7 +11,7 @@ public class InvalidLinkExceptionHandler {
 
     @ExceptionHandler(InvalidLinkException.class)
     public ResponseEntity<?> invalidLinkExceptionHandler(InvalidLinkException e) {
-        return ResponseEntity.badRequest().body(new ExceptionDTO(e.getMessage()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ExceptionDTO(e.getMessage()));
     }
 
 }
