1e817fd3c8cfb4214c19acbf43256b112fe93b37
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-05 16:20:17 -0300

    Renomeando arquivo e lidando com a exceção de quando não é encontrado o token no header para o redirecionamento

--- src/main/java/br/com/aula4/tm/linktracker/exception/LinkExceptionHandler.java
+++ src/main/java/br/com/aula4/tm/linktracker/exception/LinkExceptionHandler.java
@@ -3,15 +3,21 @@ package br.com.aula4.tm.linktracker.exception;
 import br.com.aula4.tm.linktracker.dto.ExceptionDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class InvalidLinkExceptionHandler {
+public class LinkExceptionHandler {
 
     @ExceptionHandler(InvalidLinkException.class)
     public ResponseEntity<?> invalidLinkExceptionHandler(InvalidLinkException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ExceptionDTO(e.getMessage()));
     }
 
+
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    public ResponseEntity<?> missingHeaderHandler(MissingRequestHeaderException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ExceptionDTO("Header param token not found"));
+    }
 }
