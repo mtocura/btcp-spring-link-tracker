b859072aafde5740d8f149e3e12239edc7b79384
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-05 15:54:25 -0300

    criação do controller

---
+++ src/main/java/br/com/aula4/tm/linktracker/controller/LinkController.java
@@ -0,0 +1,46 @@
+package br.com.aula4.tm.linktracker.controller;
+
+import br.com.aula4.tm.linktracker.dto.MetricDTO;
+import br.com.aula4.tm.linktracker.form.LinkForm;
+import br.com.aula4.tm.linktracker.model.service.LinkService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+@RequestMapping("/aula4/tm")
+public class LinkController {
+
+    private LinkService linkService;
+
+    public LinkController() {
+    }
+
+    @Autowired
+    public LinkController(LinkService linkService) {
+        this.linkService = linkService;
+    }
+
+    @PostMapping("/link")
+    public ResponseEntity<?> createLink(@RequestBody LinkForm linkForm) {
+        return ResponseEntity.ok(linkService.createLink(linkForm));
+    }
+
+    @PostMapping("/link/{linkId}")
+    public ResponseEntity<?> invalidateLink(@PathVariable long linkId) {
+        linkService.invalidateLink(linkId);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/link/{linkID}")
+    public RedirectView redirect(@RequestHeader("token") String token, @PathVariable long linkID) {
+        String url = linkService.redirectTo(linkID, token);
+        return new RedirectView(url);
+    }
+
+    @GetMapping("/metrics/{linkId}")
+    public ResponseEntity<?> getRedirects(@PathVariable long linkId) {
+        return ResponseEntity.ok(new MetricDTO(linkService.linkMetrics(linkId)));
+    }
+}
