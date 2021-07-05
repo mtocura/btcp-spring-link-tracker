1fd8c3d63298ca474f009f5838c8f174ab57d261
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-05 14:43:14 -0300

    Adicionando token no DTO

--- src/main/java/br/com/aula4/tm/linktracker/dto/LinkDTO.java
+++ src/main/java/br/com/aula4/tm/linktracker/dto/LinkDTO.java
@@ -2,15 +2,21 @@ package br.com.aula4.tm.linktracker.dto;
 
 public class LinkDTO {
     private Long linkId;
+    private String token;
 
     public LinkDTO() {
     }
 
-    public LinkDTO(Long linkId) {
+    public LinkDTO(Long linkId, String token) {
         this.linkId = linkId;
+        this.token = token;
     }
 
     public Long getLinkId() {
         return linkId;
+    }
+
+    public String getToken() {
+        return token;
     }
 }
