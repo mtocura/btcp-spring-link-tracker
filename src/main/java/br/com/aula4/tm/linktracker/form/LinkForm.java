063013cfc45407cf70821aee7285e600dc94d5a6
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-05 14:35:43 -0300

    Criação do Form para link

---
+++ src/main/java/br/com/aula4/tm/linktracker/form/LinkForm.java
@@ -0,0 +1,16 @@
+package br.com.aula4.tm.linktracker.form;
+
+public class LinkForm {
+    private String url;
+
+    public LinkForm() {
+    }
+
+    public LinkForm(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
