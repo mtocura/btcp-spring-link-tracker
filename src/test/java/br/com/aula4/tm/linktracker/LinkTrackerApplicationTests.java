b37b4d7547c1c7e859e7aa97b86401ce723b6e13
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-05 14:06:40 -0300

    Inicialização do projeto spring boot

---
+++ src/test/java/br/com/aula4/tm/linktracker/LinkTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aula4.tm.linktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
